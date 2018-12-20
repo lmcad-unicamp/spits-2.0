898aa9361760f8eb439cbf17aad16fde1445cd81
Caian <caian@ualberta.ca>
2018-12-20 07:05:44 -0200

    Trim spaces

--- examples/spitz-pi-2/main.cpp
+++ examples/spitz-pi-2/main.cpp
@@ -3,22 +3,22 @@
  *
  * Copyright (c) 2016 Caian Benedicto <caian@ggaunicamp.com>
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy 
- * of this software and associated documentation files (the "Software"), to 
- * deal in the Software without restriction, including without limitation the 
- * rights to use, copy, modify, merge, publish, distribute, sublicense, 
- * and/or sell copies of the Software, and to permit persons to whom the 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in 
+ *
+ * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
 
@@ -27,7 +27,7 @@
 // only once per C++ module.
 #define SPITZ_ENTRY_POINT
 
-// Spitz serial debug enables a Spitz-compliant main function to allow 
+// Spitz serial debug enables a Spitz-compliant main function to allow
 // the module to run as an executable for testing purposes.
 // #define SPITZ_SERIAL_DEBUG
 
@@ -40,9 +40,9 @@
 #include <vector>
 #include <cmath>
 
-// Parameters should not be stored inside global variables because the 
-// Spitz interface does not guarantee memory isolation between job 
-// manager, committer and workers. This can lead to a race condition when 
+// Parameters should not be stored inside global variables because the
+// Spitz interface does not guarantee memory isolation between job
+// manager, committer and workers. This can lead to a race condition when
 // each thread tries to initialize the parameters from argc and argv.
 struct parameters
 {
@@ -50,24 +50,24 @@ struct parameters
 
     int n;
     double step;
-    
+
     int split;
-    
-    parameters(int argc, const char *argv[], const std::string& who = "") : 
+
+    parameters(int argc, const char *argv[], const std::string& who = "") :
         split(1000), who(who)
     {
         if (argc < 2 || argc > 3) {
             std::cerr << "USAGE: " << argv[0] << " N" << std::endl;
             exit(1);
         }
-        
+
         n = atoi(argv[1]);
         step = 1.0 / (double)n;
     }
-    
+
     void print()
     {
-        std::cout 
+        std::cout
             << who << "N: " << n << std::endl
             << who << "step: " << step << std::endl;
     }
@@ -85,7 +85,7 @@ public:
         double pi_1, pi_2, pi_err;
         int r;
 
-        // Let's do two steps: Compute pi with the provided number of 
+        // Let's do two steps: Compute pi with the provided number of
         // iteractions and then halving the number of iteractions
 
         std::stringstream ss;
@@ -112,7 +112,7 @@ public:
             exit(1);
         }
 
-        // We know the passed data is a double, 
+        // We know the passed data is a double,
         // collect the first value of pi
         final_result >> pi_1;
 
@@ -126,7 +126,7 @@ public:
             exit(1);
         }
 
-        // We know the passed data is a double, 
+        // We know the passed data is a double,
         // collect the second value of pi
         final_result >> pi_2;
 
@@ -134,11 +134,11 @@ public:
         pi_err = std::abs(pi_2 - pi_1);
 
         std::cout.precision(17);
-        std::cout << "[SM] First pi value is: " << std::fixed 
+        std::cout << "[SM] First pi value is: " << std::fixed
             << pi_1 << std::endl
-            << "[SM] Second pi value is: " << std::fixed 
+            << "[SM] Second pi value is: " << std::fixed
             << pi_2 << std::endl
-            << "[SM] The error is: " << std::fixed 
+            << "[SM] The error is: " << std::fixed
             << pi_err << std::endl;
 
         return 0;
@@ -151,37 +151,37 @@ class job_manager : public spitz::job_manager
 private:
     parameters p;
     int i;
-    
+
 public:
-    job_manager(int argc, const char *argv[], spitz::istream& jobinfo) : 
+    job_manager(int argc, const char *argv[], spitz::istream& jobinfo) :
         p(argc, argv, "[JM] "), i(0)
     {
         p.print();
-        
+
         std::cout << "[JM] Job manager created." << std::endl;
     }
-    
+
     bool next_task(const spitz::pusher& task)
     {
         spitz::ostream o;
-        
+
         if (i >= p.n)
             return false;
-        
+
         // Push the current step
         o << i;
 
         std::cout << "[JM] Generated task for i = " << i << "." << std::endl;
-        
+
         // Increment the step by the amount to be packed for each worker
         i += p.split;
-        
+
         // Serialize and push the task
         task.push(o);
-        
+
         return true;
     }
-    
+
     ~job_manager()
     {
     }
@@ -189,33 +189,33 @@ public:
 
 // This class executes tasks, preferably without modifying its internal state
 // because this can lead to a break of idempotence between tasks. The 'run'
-// method will not impose a 'const' behavior to allow libraries that rely 
+// method will not impose a 'const' behavior to allow libraries that rely
 // on changing its internal state, for instance, OpenCL (see clpi example).
 class worker : public spitz::worker
 {
 private:
     parameters p;
-    
+
 public:
     worker(int argc, const char *argv[]) : p(argc, argv, "[WK] ")
     {
         p.print();
         std::cout << "[WK] Worker created." << std::endl;
     }
-    
+
     int run(spitz::istream& task, const spitz::pusher& result)
     {
         // Data stream used to store the output
         spitz::ostream o;
-        
+
         // Read the current step from the task
         int i, batch;
         task >> i;
         batch = i;
-        
+
         // Get the number of steps to compute
         int n = std::min<int>(p.n, i + p.split);
-        
+
         // Compute each term of the pi expansion
         for ( ; i < n; i++) {
             double x = (static_cast<double>(i) + 0.5) * p.step;
@@ -223,56 +223,56 @@ public:
             // Add the term to the data stream
             o << x;
         }
-        
+
         // Send the result to the Spitz runtime
         result.push(o);
-        
-        std::cout << "[WK] Processed  batch " << batch << "." << std::endl;
+
+        std::cout << "[WK] Processed batch " << batch << "." << std::endl;
         return 0;
     }
 };
 
-// This class is responsible for merging the result of each individual task 
-// and, if necessary, to produce the final result after all of the task 
+// This class is responsible for merging the result of each individual task
+// and, if necessary, to produce the final result after all of the task
 // results have been received.
 class committer : public spitz::committer
 {
 private:
     parameters p;
     double pival;
-    
+
 public:
-    committer(int argc, const char *argv[], spitz::istream& jobinfo) : 
+    committer(int argc, const char *argv[], spitz::istream& jobinfo) :
         p(argc, argv, "[CO] "), pival(0.0)
     {
         p.print();
         std::cout << "[CO] Committer created." << std::endl;
     }
-    
+
     int commit_task(spitz::istream& result)
     {
         std::cout << "[CO] Committing result..." << std::endl;
-        
+
         // Accumulate each term of the expansion
         while(result.has_data()) {
             double d;
             result >> d;
             pival += d;
         }
-        
+
         return 0;
     }
-    
-    int commit_job(const spitz::pusher& final_result) 
+
+    int commit_job(const spitz::pusher& final_result)
     {
         // Our result stream
         spitz::ostream o;
 
-        // Multiply the sum of terms by the step to get the 
-        // final result of pi and print it with the maximum 
+        // Multiply the sum of terms by the step to get the
+        // final result of pi and print it with the maximum
         // precision possible
         pival = pival * p.step;
-        
+
         // Push the result
         o << pival;
         final_result.push(o);
@@ -281,7 +281,7 @@ public:
 
     ~committer()
     {
-        
+
     }
 };
 
@@ -294,18 +294,18 @@ public:
         return new spitz_main();
     }
 
-    spitz::job_manager *create_job_manager(int argc, const char *argv[], 
+    spitz::job_manager *create_job_manager(int argc, const char *argv[],
         spitz::istream& jobinfo)
     {
         return new job_manager(argc, argv, jobinfo);
     }
-    
+
     spitz::worker *create_worker(int argc, const char *argv[])
     {
         return new worker(argc, argv);
     }
-    
-    spitz::committer *create_committer(int argc, const char *argv[], 
+
+    spitz::committer *create_committer(int argc, const char *argv[],
         spitz::istream& jobinfo)
     {
         return new committer(argc, argv, jobinfo);
--- spitz-include/ccpp/spitz/stream.hpp
+++ spitz-include/ccpp/spitz/stream.hpp
@@ -4,22 +4,22 @@
  * Copyright (c) 2015 Caian Benedicto <caian@ggaunicamp.com>
  * Copyright (c) 2014 Ian Liu Rodrigues <ian.liu@ggaunicamp.com>
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy 
- * of this software and associated documentation files (the "Software"), to 
- * deal in the Software without restriction, including without limitation the 
- * rights to use, copy, modify, merge, publish, distribute, sublicense, 
- * and/or sell copies of the Software, and to permit persons to whom the 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in 
+ *
+ * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
 
@@ -37,12 +37,12 @@
 #include <algorithm>
 
 namespace spitz {
-    
-    class ostream 
+
+    class ostream
     {
     private:
         std::vector<char> pdata;
-        
+
         static uint64_t spitz_htonll(uint64_t x)
         {
             if (htons(1) == 1)
@@ -53,26 +53,26 @@ namespace spitz {
             uint64_t nhi = htonl(hi);
             return nhi << 32 | nlo;
         }
-        
+
         template<typename T> void push_back(const T& v)
         {
             size_t s = this->pdata.size();
             this->pdata.resize(s + sizeof(T));
             *reinterpret_cast<T*>(this->pdata.data() + s) = v;
         }
-        
+
         template<typename T> uint16_t hton16(const T& v)
         {
             const char* p = reinterpret_cast<const char*>(&v);
             return htons(*reinterpret_cast<const uint16_t*>(p));
         }
-        
+
         template<typename T> uint32_t hton32(const T& v)
         {
             const char* p = reinterpret_cast<const char*>(&v);
             return htonl(*reinterpret_cast<const uint32_t*>(p));
         }
-        
+
         template<typename T> uint64_t hton64(const T& v)
         {
             const char* p = reinterpret_cast<const char*>(&v);
@@ -81,7 +81,7 @@ namespace spitz {
 
     public:
         ostream() : pdata() { }
-        
+
         void write_bool(const bool& v) { push_back((char)(v ? 1 : 0)); }
         void write_char(const int8_t& v) { push_back(v); }
         void write_byte(const uint8_t& v) { push_back(v); }
@@ -94,18 +94,18 @@ namespace spitz {
         void write_longlong(const int64_t& v) { push_back(hton64(v)); }
         void write_ulonglong(const uint64_t& v) { push_back(hton64(v)); }
         void write_string(const std::string& v)
-        { 
+        {
             const char *p = v.c_str();
-            do { write_char(*p); } 
+            do { write_char(*p); }
             while (*p++);
         }
         void write_data(const void* pdata, size_t size)
         {
-            std::copy(reinterpret_cast<const char*>(pdata), 
-                reinterpret_cast<const char*>(pdata) + size, 
+            std::copy(reinterpret_cast<const char*>(pdata),
+                reinterpret_cast<const char*>(pdata) + size,
                 std::back_inserter(this->pdata));
         }
-        
+
         ostream& operator<<(const float& v)       { write_float(v); return *this; }
         ostream& operator<<(const double& v)      { write_double(v); return *this; }
         ostream& operator<<(const int8_t& v)      { write_char(v); return *this; }
@@ -119,23 +119,23 @@ namespace spitz {
         ostream& operator<<(const bool& v)        { write_bool(v); return *this; }
         ostream& operator<<(const std::string& v) { write_string(v); return *this; }
 
-        size_t pos() const 
+        size_t pos() const
         {
             return this->pdata.size();
         }
-        
+
         const void* data() const
         {
             return this->pdata.data();
         }
     };
-    
-    class istream 
+
+    class istream
     {
     private:
         const char* pdata;
         size_t sz, pos;
-        
+
         static uint64_t spitz_ntohll(uint64_t x)
         {
             if (ntohs(1) == 1)
@@ -146,7 +146,7 @@ namespace spitz {
             uint64_t nhi = htonl(hi);
             return nhi << 32 | nlo;
         }
-        
+
         char get1()
         {
             ensure_size(1);
@@ -178,7 +178,7 @@ namespace spitz {
             this->pos += 8;
             return spitz_ntohll(v);
         }
-        
+
         template<typename T> T get_as()
         {
             ensure_size(sizeof(T));
@@ -186,7 +186,7 @@ namespace spitz {
             this->pos += sizeof(T);
             return v;
         }
-        
+
         void ensure_size(size_t n)
         {
             if (this->sz - this->pos < n)
@@ -194,20 +194,20 @@ namespace spitz {
         }
 
     public:
-        istream() : 
-            pdata(reinterpret_cast<const char*>(0)), 
-            sz(0), 
-            pos(0) 
-        { 
+        istream() :
+            pdata(reinterpret_cast<const char*>(0)),
+            sz(0),
+            pos(0)
+        {
         }
-            
-        istream(const void *data, const size_t& size) : 
-            pdata(reinterpret_cast<const char*>(data)), 
-            sz(size), 
-            pos(0) 
-        { 
+
+        istream(const void *data, const size_t& size) :
+            pdata(reinterpret_cast<const char*>(data)),
+            sz(size),
+            pos(0)
+        {
         }
-        
+
         bool read_bool() { uint8_t v = get1(); return v ? true : false; }
         int8_t read_char() { return get1(); }
         uint8_t read_byte() { char v = get1(); return *((int8_t*)&v); }
@@ -220,7 +220,7 @@ namespace spitz {
         int64_t read_longlong() { uint64_t v = get8(); return *((int64_t*)(char*)&v); }
         uint64_t read_ulonglong() { return get8(); }
         std::string read_string()
-        { 
+        {
             std::stringstream ss;
             char c;
             while((c = read_char()))
@@ -230,10 +230,10 @@ namespace spitz {
         void read_data(void* pdata, size_t size)
         {
             ensure_size(size);
-            
-            std::copy(this->pdata + this->pos, this->pdata + this->pos + size, 
+
+            std::copy(this->pdata + this->pos, this->pdata + this->pos + size,
                 reinterpret_cast<char*>(pdata));
-            
+
             this->pos += size;
         }
 
@@ -250,12 +250,12 @@ namespace spitz {
         istream& operator>>(bool& v)        { v = read_bool(); return *this; }
         istream& operator>>(std::string& v) { v = read_string(); return *this; }
 
-        size_t size() const 
+        size_t size() const
         {
             return this->sz;
         }
-        
-        bool has_data() const 
+
+        bool has_data() const
         {
             return this->pos < this->sz;
         }
